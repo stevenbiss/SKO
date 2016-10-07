f58f6a69863d0af41224e07c1251d4d10cb725ee
Steven Biss <biss@adobe.com>
2016-10-07 13:48:18 +0100

    Android

---
+++ sko/BasicPlayerSample-android-2.0.0/app/src/main/java/com/adobe/primetime/va/samples/Configuration.java
@@ -0,0 +1,25 @@
+/*
+ * ADOBE SYSTEMS INCORPORATED
+ * Copyright 2014 Adobe Systems Incorporated
+ * All Rights Reserved.
+
+ * NOTICE:  Adobe permits you to use, modify, and distribute this file in accordance with the
+ * terms of the Adobe license agreement accompanying it.  If you have received this file from a
+ * source other than Adobe, then your use, modification, or distribution of it requires the prior
+ * written permission of Adobe.
+ */
+package com.adobe.primetime.va.samples;
+
+public class Configuration {
+    public static final String PLAYER_NAME = "SKO ANDROID VOD";
+    public static final String VIDEO_ID = "SKO ANDROID VOD";
+    public static final String VIDEO_NAME = "SKO ANDROID VOD";
+
+    public static final Double VIDEO_LENGTH = 600D;
+
+    public static final String HEARTBEAT_TRACKING_SERVER = "http://stevenbiss.hb.omtrdc.net";
+    public static final String HEARTBEAT_PUBLISHER       = "TEST";
+    public static final String HEARTBEAT_CHANNEL         = "test-channel";
+    public static final String HEARTBEAT_OVP             = "test-ovp";
+    public static final String HEARTBEAT_SDK             = "VHL 2.x Sample Player";
+}
